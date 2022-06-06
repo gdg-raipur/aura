bf26574a062cf808aab366c357809b009d4f0698
Kuldeep Pisda <22424149+kdpisda@users.noreply.github.com>
2022-06-06 05:39:10 +0530

    Serup projects and CI/CD

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,31 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';   
-import 'firebase/storage';
-import 'firebase/firestore';
-import 'firebase/messaging';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/firestore";
+import "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+  apiKey: "AIzaSyAlwSZrtuLif8Yz32fbkHEDHnp3DHKEL1I",
+  authDomain: "aura-d175c.firebaseapp.com",
+  databaseURL: "https://aura-d175c.firebaseio.com",
+  projectId: "aura-d175c",
+  storageBucket: "aura-d175c.appspot.com",
+  messagingSenderId: "414686210631",
+  appId: "1:414686210631:web:a45b9ff076d23e0b89df62",
+  measurementId: "G-51X9LVVLD4",
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().enablePersistence({synchronizeTabs:!0}).catch(()=>{console.warn("DB offline support not available")})
-export default{
-  notificationSupported:firebase.messaging.isSupported(),
-  messaging: (firebase.messaging.isSupported())?firebase.messaging():null,
+firebase
+  .firestore()
+  .enablePersistence({ synchronizeTabs: !0 })
+  .catch(() => {
+    console.warn("DB offline support not available");
+  });
+export default {
+  notificationSupported: firebase.messaging.isSupported(),
+  messaging: firebase.messaging.isSupported() ? firebase.messaging() : null,
   firestore: firebase.firestore(),
-  auth:firebase.auth(),
-  storage:firebase.storage(),
+  auth: firebase.auth(),
+  storage: firebase.storage(),
 };
